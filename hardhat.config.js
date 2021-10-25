c591611db7a6715c3104895be4b7fe571da5debe
Ray Adamas <novumrembey@gmail.com>
2021-10-24 23:28:00 -0500

    remove comment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,3 @@
-/*
-require("@nomiclabs/hardhat-waffle");
-
-module.exports = {
-  solidity: "0.8.0",
-  networks: {
-    rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/bJmicFhD6yic2iejGc8NuD4djCQT91dH',
-      accounts: ['81d471480523cb46aad7cbfc3b4a47f1b099ad142f46f87a8a5851405ba3cd20']
-    },
-  },
-};
-*/
-
 require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
 
